500060f6cf894db5d7960079e0918a39e0073cb0
TianxinSun <93703023+TianxinSun@users.noreply.github.com>
2022-07-14 19:04:37 +0800

    Sam

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 重置样式库
+import 'normalize.css'
+// 自己项目的重置样式和公用样式
+import '@/assets/styles/common.less'
 
 createApp(App)
   .use(store)
